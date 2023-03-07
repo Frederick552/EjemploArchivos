cacd9f6359673e32819a8a54f6ccbf6747ceac08
JADPA-14 <JADPA-14@DESKTOP-4CP78BB>
2023-03-07 10:24:36 -0600

    Agregar archivos de proyecto.

---
+++ EjemploArchivos/frmEjemploArchivo.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace EjemploArchivos
+{
+    partial class frmEjemploArchivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduzca el archivo directorio";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(39, 61);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(211, 23);
+            this.txtEntrada.TabIndex = 1;
+            this.txtEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntrada_KeyDown);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Location = new System.Drawing.Point(39, 110);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(211, 215);
+            this.txtSalida.TabIndex = 2;
+            // 
+            // frmEjemploArchivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 387);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEjemploArchivo";
+            this.Text = "EjemploArchivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtEntrada;
+        private TextBox txtSalida;
+    }
+}
---
+++ EjemploArchivos/frmEjemploArchivo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploArchivos
+{
+    public partial class frmEjemploArchivo : Form
+    {
+        public frmEjemploArchivo()
+        {
+            InitializeComponent();
+        }
+        // Se invoca cuando el usuario oprime una tecla
+        private void txtEntrada_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Determina si el usuario oprimio la tecla Intro
+            if(e.KeyCode == Keys.Enter)
+            {
+                string nombreArchivo; // nombre del archivo
+
+                // Obtiene el archivo o directorio especificado del usuario
+                nombreArchivo = txtEntrada.Text;
+
+                // Determina si nombreArchivo es un archivo
+                if (File.Exists(nombreArchivo))
+                {
+                    txtSalida.Text = obtenerInformacion(nombreArchivo);
+                    try
+                    {
+                        StreamReader sr = new StreamReader(nombreArchivo);
+                        txtSalida.Text += sr.ReadToEnd();
+                    }
+                    // Maneja exception si el streamreader no esta disponible
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error al leer el archivo", "Error de archivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        throw;  
+                    }
+                }
+                else if (Directory.Exists(nombreArchivo))
+                {
+                    string[] listaDirectorios; //arreglo para los directorios
+
+                    txtSalida.Text = obtenerInformacion(nombreArchivo);
+                    listaDirectorios = Directory.GetDirectories(nombreArchivo);
+
+                    txtSalida.Text += "\r\n\r\nContenido del directorio:\r\n";
+
+                    for (int i = 0; i < listaDirectorios.Length; i++)
+                    {
+                        txtSalida.Text += listaDirectorios[i] + "\r\n";
+                    }
+                }
+                else
+                {
+                    // Notifica al usuario 
+                    MessageBox.Show(txtEntrada.Text + "no existe", "Error de archivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } 
+            }
+        }
+        // Obtiene informacion sobre el archivo o directorio
+        private string obtenerInformacion(string nombreArchivo)
+        {
+            String informacion;
+            // Imprime mensaje indicado que existe en el archivo o directorio
+            informacion = nombreArchivo + " existe\r\n\r\n";
+
+            // imprimir
+            informacion += "creacion:" +
+                File.GetCreationTime(nombreArchivo) + "\r\n";
+
+            informacion += "Ultima modificacion:" +
+                File.GetLastWriteTime(nombreArchivo) + "\r\n";
+
+            informacion += "Ultimo acceso:" +
+                File.GetLastAccessTime(nombreArchivo) + "\r\n" + "\r\n";
+
+            return informacion;
+        }
+    }
+}
